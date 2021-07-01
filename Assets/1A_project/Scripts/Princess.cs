6f46686bdf2e1b8774e0d190a43a0a7db69dfa53
Shelekhan Dmitry <shedidraco@gmail.com>
2021-07-01 16:47:20 +0300

    All Work

---
+++ Assets/1A_project/Scripts/Princess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Princess : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+	{
+    	Knight knight = collider.gameObject.GetComponent<Knight>();
+
+     	if (knight != null)
+    	{
+        		//Рыцарь нашел принцессу
+    	}
+	}
+}
