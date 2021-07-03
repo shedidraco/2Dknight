25857baaa60d2a6bf85d5bc6c48c937eefe8c625
Shelekhan Dmitry <shedidraco@gmail.com>
2021-07-03 22:51:44 +0300

    PZ14 Finished

---
+++ Assets/1A_project/Scripts/TriggerFinishGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerFinishGame : MonoBehaviour
+{ private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        Knight knight = collider.gameObject.GetComponent<Knight>();
+        if (knight != null)
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }     
+    }
+}
