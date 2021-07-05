347f8591df6d6cf32e3fd9f4f6958afaf7898833
Shelekhan Dmitry <shedidraco@gmail.com>
2021-07-05 21:57:22 +0300

    add Musik

--- Assets/1A_project/Scripts/Audio.cs
+++ Assets/1A_project/Scripts/Audio.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable] public class Audio : MonoBehaviour
+[System.Serializable] public class Audio
 {
 #region Private_Variables
 
